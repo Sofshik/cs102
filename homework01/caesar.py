e09f40696cf08e4960e91fbdc58c0531ef503385
Sofshik <sofshik@gmail.com>
2020-10-05 23:33:36 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,46 +1,54 @@
 import typing as tp
 
 
-def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
+def encrypt_caesar(plaintext: str, shift: int) -> str:
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    rawtext = list(plaintext)
+    for letter in rawtext:
+        letter = ord(letter)
+        if (letter >= ord('A') and letter <= ord('Z')) or (letter >= ord('a') and letter <= ord('z')):
+            if (letter > (ord('Z') - shift)) and (letter <= ord('Z')):
+                letter -= 26
+            elif (letter > (ord('z') - shift)) and (letter <= ord('z')):
+                letter -= 26
+            letter += shift
+            letter = chr(letter)
+            ciphertext = ciphertext + letter 
+        else:
+            letter = chr(letter)
+            ciphertext = ciphertext + letter 
     return ciphertext
 
-
-def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
+def decrypt_caesar(ciphertext: str, shift: int) -> str:
     plaintext = ""
-    # PUT YOUR CODE HERE
+    rawtext = list(ciphertext)
+    for letter in rawtext:
+        letter = ord(letter)
+        if (letter >= ord('A') and letter <= ord('Z')) or (letter >= ord('a') and letter <= ord('z')):
+            if (letter >= ord('A')) and (letter < (ord('A') + shift)):
+                letter += 26 
+            elif (letter >= ord('a')) and (letter < (ord('a') + shift)):
+                letter += 26
+            letter -= shift
+            letter = chr(letter)
+            plaintext = plaintext + letter
+        else:
+            letter = chr(letter)
+            plaintext = plaintext + letter
     return plaintext
 
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
+d = {"python", "java", "ruby"}
+def caesar_breaker(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
-    # PUT YOUR CODE HERE
+    rawtext = list(ciphertext.lower())
+    for element in dictionary:
+        rawelement = list(element)
+        sub1 = ord(rawtext[0]) - ord(rawelement[0])
+        sub2 = ord(rawtext[1]) - ord(rawelement[1])
+        if sub1 < 0:
+            sub1 += 26
+        if sub2 < 0:
+            sub2 += 26
+        if sub1 == sub2:
+            best_shift = sub1
     return best_shift
