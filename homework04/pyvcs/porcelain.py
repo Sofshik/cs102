7b739c0d8b18f5cdb49a93478e30b18bbce2eb9a
Sofshik <sofshik@gmail.com>
2020-12-15 20:40:48 +0300

    Fixed isort

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -3,13 +3,7 @@ import pathlib
 import typing as tp
 
 from pyvcs.index import read_index, update_index  # type: ignore
-from pyvcs.objects import (
-    commit_parse,
-    find_object,
-    find_tree_files,  # type: ignore
-    read_object,
-    read_tree,
-)
+from pyvcs.objects import commit_parse, find_object, find_tree_files, read_object, read_tree
 from pyvcs.refs import get_ref, is_detached, resolve_head, update_ref  # type: ignore
 from pyvcs.tree import commit_tree, write_tree  # type: ignore
 
