2698da5b33b29ed5aff3f6d48fb92152a96500ad
Sofshik <sofshik@gmail.com>
2021-03-27 01:20:45 +0300

    First attempt

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,17 +1,48 @@
+from collections import defaultdict
+from math import log
+
+
 class NaiveBayesClassifier:
 
-    def __init__(self, alpha):
-        pass
+    def __init__(self, alpha: int = 1e-5):
+        self.d = 0
+        self.word = defaultdict(lambda: 0)
+        self.class_words = defaultdict(lambda: 0)
+        self.y = defaultdict(lambda: 0)
+        self.alpha = alpha
 
     def fit(self, X, y):
         """ Fit Naive Bayes classifier according to X, y. """
-        pass
+        for x in range(len(X)):
+            self.y[y[x]] += 1
+            words = X[x].split()
+            for wrd in words:
+                self.word[wrd] += 1
+                self.class_words[wrd, y[x]] += 1
+
+        for cl in self.y:
+            self.y[cl] /= len(X)
+
+        self.d = len(self.word)
 
     def predict(self, X):
         """ Perform classification on an array of test vectors X. """
-        pass
+        assert len(self.y) > 0
+        return max(
+            self.y.keys(),
+            key=lambda cl: log(self.y[cl])
+            + sum(
+                log((self.class_words[wrd, cl] + self.alpha) / (self.word[wrd] + self.alpha * self.d))
+                for wrd in X.split()
+            ),
+        )
 
     def score(self, X_test, y_test):
         """ Returns the mean accuracy on the given test data and labels. """
-        pass
+        prediction = []
+        for one in X_test:
+            prediction.append(self.predict(one))
+        return sum(0 if prediction[k] != y_test[k] else 1 for k in range(len(X_test))) / len(
+            X_test
+        )
 
--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -2,9 +2,10 @@ from bottle import (
     route, run, template, request, redirect
 )
 
-from scrapper import get_news
+from scraputils import get_news
 from db import News, session
 from bayes import NaiveBayesClassifier
+import string
 
 
 @route("/news")
@@ -16,21 +17,60 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    query = request.query.decode()
+    id = int(query["id"])
+    label = query["label"]
+    s = session()
+    s.query(News.id).filter(News.id == id).update({News.label: label})
+    s.commit()
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    news = get_news("https://news.ycombinator.com/", 3)
+    s = session()
+    for new in news:
+        if len(s.query(News.author).filter_by(author='author').all()) == 0 or len(s.query(News.title).filter_by(title='title').all()) == 0:
+            s.add(
+                News(
+                    title=new["title"],
+                    author=new["author"],
+                    url=new["url"],
+                    points=new["points"],
+                    comments=new["comments"],
+                )
+            )
+    s.commit()
     redirect("/news")
+
+
+def clean(s):
+    translator = str.maketrans('', '', string.punctuation)
+    return s.translate(translator)
 
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    s = session()
+    model = NaiveBayesClassifier()
+    train = s.query(News).filter(News.label != None).all()
+    model.fit([clean(news.title).lower() for news in train], [news.label for news in train])
+    test = s.query(News).filter(News.label == None).all()
+    return template(
+        'news_template',
+        rows=sorted(test, key=lambda news: get_l(model.predict(clean(news.title).lower()))),
+    )
+
+
+def get_l(label):
+    if label == "never":
+        return 2
+    elif label == "maybe":
+        return 1
+    elif label == "good":
+        return 0
 
 
 if __name__ == "__main__":
     run(host="localhost", port=8080)
-
