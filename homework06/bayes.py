d5c03832a601db662c0ff329cf6f63d2333ecbdd
Sofshik <sofshik@gmail.com>
2021-04-06 21:08:29 +0300

    Classifying.py added

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,47 +1,84 @@
-from collections import defaultdict
-from math import log
+from collections import Counter
+from math import e, log
 
 
 class NaiveBayesClassifier:
-    def __init__(self, alpha: float = 1e-5):
-        self.d = 0
-        self.word = defaultdict(lambda: 0)  # type: ignore
-        self.class_words = defaultdict(lambda: 0)  # type: ignore
-        self.y = defaultdict(lambda: 0)  # type: ignore
+    def __init__(self, alpha: float = 1e-5) -> None:
+        # self.d = 0
+        # self.word = defaultdict(lambda: 0)  # type: ignore
+        # self.class_words = defaultdict(lambda: 0)  # type: ignore
+        # self.y = defaultdict(lambda: 0)  # type: ignore
         self.alpha = alpha
+        self.model: dict = {
+            "labels": {},
+            "words": {},
+        }
 
-    def fit(self, X, y):
-        """ Fit Naive Bayes classifier according to X, y. """
-        for x in range(len(X)):
-            self.y[y[x]] += 1
-            words = X[x].split()
-            for wrd in words:
-                self.word[wrd] += 1
-                self.class_words[wrd, y[x]] += 1
+    def fit(self, X, y) -> None:
+        """ Fit Naive Bayes classifier according to X, y."""
+        words_list = []
+        for sentence, lable in zip(X, y):
+            for word in sentence.split():
+                words_list.append((word, lable))
 
-        for cl in self.y:
-            self.y[cl] /= len(X)
+        self.words_labels = Counter(words_list)
+        print("words_labels", self.words_labels)
+        self.counted_labels: dict = dict(Counter(y))
+        print("counted_labels", self.counted_labels)
+        words = [word for sentence in X for word in sentence.split()]
+        self.counted_words = dict(Counter(words))
+        print("counted_words", self.counted_words)
+        self.model = {
+            "labels": {},
+            "words": {},
+        }
 
-        self.d = len(self.word)
+        for var_label in self.counted_labels:
+            params = {
+                "label_count": self.count_words(var_label),
+                "probability": self.counted_labels[var_label] / len(y),
+            }
+            self.model["labels"][var_label] = params
+        for word in self.counted_words:
+            params = {}
+            for var_label in self.counted_labels:
+                params[var_label] = self.smoothing(word, var_label)
+            self.model["words"][word] = params
 
-    def predict(self, X):
+    def predict(self, X) -> str:
         """ Perform classification on an array of test vectors X. """
-        assert len(self.y) > 0
-        return max(
-            self.y.keys(),
-            key=lambda cl: log(self.y[cl])
-            + sum(
-                log(
-                    (self.class_words[wrd, cl] + self.alpha)
-                    / (self.word[wrd] + self.alpha * self.d)
-                )
-                for wrd in X.split()
-            ),
-        )
+        words = X.split()
+        prob_labels = []
+        for cur_label in self.model["labels"]:
+            probability = self.model["labels"][cur_label]["probability"]
+            total_score = log(probability, e)
+            for word in words:
+                word_dict = self.model["words"].get(word, None)
+                if word_dict:
+                    total_score += log(word_dict[cur_label], e)
+            prob_labels.append((total_score, cur_label))
+        _, answer = max(prob_labels)
+        return answer
 
-    def score(self, X_test, y_test):
+    def score(self, X_test, y_test) -> float:
         """ Returns the mean accuracy on the given test data and labels. """
         prediction = []
         for one in X_test:
             prediction.append(self.predict(one))
         return sum(0 if prediction[k] != y_test[k] else 1 for k in range(len(X_test))) / len(X_test)
+
+    def smoothing(self, word, cur_label) -> float:
+        """ Возвращает сглаженную вероятность со словом и лейблом. """
+        nc = self.model["labels"][cur_label]["label_count"]
+        nic = self.words_labels.get((word, cur_label), 0)
+        counted_len = len(self.counted_words)
+        alpha = self.alpha
+        return (nic + alpha) / (nc + alpha * counted_len)
+
+    def count_words(self, cur_label) -> int:
+        """ Возвращает посчитанные слова с присвоенными лейблами. """
+        count = 0
+        for word, label_name in self.words_labels:
+            if cur_label == label_name:
+                count += self.words_labels[(word, cur_label)]
+        return count
